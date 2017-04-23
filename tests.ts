40b3f2e7bff15cc29aa8052cf19db929673b79cc
Peli de Halleux <jhalleux@microsoft.com>
2017-04-22 20:13:43 -0700

    cleaned of api

--- tests.ts
+++ tests.ts
@@ -1,1 +1,6 @@
 // tests go here; this will not be compiled when this package is used as a library
+midi.playNote(400, 500);
+for (let i = 0; i < 127; ++i)
+    midi.drum(i)
+let i = midi.inputChannel(0);
+i.setInstrument(MidiInstrument.Banjo);
