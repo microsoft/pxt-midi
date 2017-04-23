13a15e7d864dd8819b993f257e7b6ff8e822f1c0
Peli de Halleux <jhalleux@microsoft.com>
2017-04-23 07:05:38 -0700

    moving notes to static global

--- midi.ts
+++ midi.ts
@@ -237,7 +237,6 @@ namespace midi {
         /**
          * Channel Pressure (After-touch). This message is most often sent by pressing down on the key after it "bottoms out".
          * This message is different from polyphonic after-touch. Use this message to send the single greatest pressure value (of all the current depressed keys).
-         * (vvvvvvv) is the pressure value.
          * @param pressure the pressure value
          */
         //% blockId=midi_after_touch block="%this|after touch %pressure"
@@ -310,6 +309,7 @@ namespace midi {
         inputChannel(0).pitchBend(bend);
     }
 
+    const notes = [8, 9, 9, 10, 10, 11, 12, 12, 13, 14, 15, 15, 16, 17, 18, 19, 21, 22, 23, 24, 26, 28, 29, 31, 33, 35, 37, 39, 41, 44, 46, 49, 52, 55, 58, 62, 65, 69, 73, 78, 82, 87, 92, 98, 104, 110, 117, 123, 131, 139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523, 554, 587, 622, 659, 698, 740, 784, 831, 880, 932, 988, 1047, 1109, 1175, 1245, 1319, 1397, 1480, 1568, 1661, 1760, 1865, 1976, 2093, 2217, 2349, 2489, 2637, 2794, 2960, 3136, 3322, 3520, 3729, 3951, 4186, 4435, 4699, 4978, 5274, 5588, 5920, 6272, 6645, 7040, 7459, 7902, 8372, 8870, 9397, 9956, 10548, 11175, 11840, 12544];
     /**
      * Maps a frequency to a note key
      * @param frequency
@@ -317,7 +317,6 @@ namespace midi {
     //% blockId=midi_frequency_to_key block="key at %frequency=device_note" useEnumVal=1
     //% advanced=true
     export function frequencyToKey(frequency: number): number {
-        const notes = [8, 9, 9, 10, 10, 11, 12, 12, 13, 14, 15, 15, 16, 17, 18, 19, 21, 22, 23, 24, 26, 28, 29, 31, 33, 35, 37, 39, 41, 44, 46, 49, 52, 55, 58, 62, 65, 69, 73, 78, 82, 87, 92, 98, 104, 110, 117, 123, 131, 139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523, 554, 587, 622, 659, 698, 740, 784, 831, 880, 932, 988, 1047, 1109, 1175, 1245, 1319, 1397, 1480, 1568, 1661, 1760, 1865, 1976, 2093, 2217, 2349, 2489, 2637, 2794, 2960, 3136, 3322, 3520, 3729, 3951, 4186, 4435, 4699, 4978, 5274, 5588, 5920, 6272, 6645, 7040, 7459, 7902, 8372, 8870, 9397, 9956, 10548, 11175, 11840, 12544];
         let left = 0, right = 0x7f, mid = 69;
 
         while (right - left > 1) {
