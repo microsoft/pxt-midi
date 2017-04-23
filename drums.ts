1547a603afce0a166e2fa00ed79ea2e715c0291d
Peli de Halleux <jhalleux@microsoft.com>
2017-04-23 05:35:09 -0700

    fixed pitch bend

---
+++ drums.ts
@@ -0,0 +1,96 @@
+enum DrumSound {
+    //% block="Acoustic Bass Drum" enumval=35 blockIdentity="midi.drumSound"
+    AcousticBassDrum = 35,
+    //% block="Bass Drum 1" enumval=36 blockIdentity="midi.drumSound"
+    BassDrum1 = 36,
+    //% block="Side Stick" enumval=37 blockIdentity="midi.drumSound"
+    SideStick = 37,
+    //% block="Acoustic Snare" enumval=38 blockIdentity="midi.drumSound"
+    AcousticSnare = 38,
+    //% block="Hand Clap" enumval=39 blockIdentity="midi.drumSound"
+    HandClap = 39,
+    //% block="Electric Snare" enumval=40 blockIdentity="midi.drumSound"
+    ElectricSnare = 40,
+    //% block="Low Floor Tom" enumval=41 blockIdentity="midi.drumSound"
+    LowFloorTom = 41,
+    //% block="Closed Hi Hat" enumval=42 blockIdentity="midi.drumSound"
+    ClosedHiHat = 42,
+    //% block"High Floor Tom" enumval=43 blockIdentity="midi.drumSound"
+    HighFloorTom = 43,
+    //% block="Pedal Hi-Hat" enumval=44 blockIdentity="midi.drumSound"
+    PedalHiHat = 44,
+    //% block="Low Tom" enumval=45 blockIdentity="midi.drumSound"
+    LowTom = 45,
+    //% block="Open Hi-Hat" enumval=46 blockIdentity="midi.drumSound"
+    OpenHiHat = 46,
+    //% block="Low-Mid Tom" enumval=47 blockIdentity="midi.drumSound"
+    LowMidTom = 47,
+    //% block="Hi-Mid Tom" enumval=48 blockIdentity="midi.drumSound"
+    HiMidTom = 48,
+    //% block="Crash Cymbal 1" enumval=49 blockIdentity="midi.drumSound"
+    CrashCymbal1 = 49,
+    //% block="High Tom" enumval=50 blockIdentity="midi.drumSound"
+    HighTom = 50,
+    //% block="Ride Cymbal 1" enumval=51 blockIdentity="midi.drumSound"
+    RideCymbal1 = 51,
+    //% block="Chinese Cymbal" enumval=52 blockIdentity="midi.drumSound"
+    ChineseCymbal = 52,
+    //% block="Ride Bell" enumval=53 blockIdentity="midi.drumSound"
+    RideBell = 53,
+    //% block="Tambourine" enumval=54 blockIdentity="midi.drumSound"
+    Tambourine = 54,
+    //% block="Splash Cymbal" enumval=55 blockIdentity="midi.drumSound"
+    SplashCymbal = 55,
+    //% block="Cowbell" enumval=56 blockIdentity="midi.drumSound"
+    Cowbell = 56,
+    //% block="Crash Cymbal 2" enumval=57 blockIdentity="midi.drumSound"
+    CrashCymbal2 = 57,
+    //% block="Vibraslap" enumval=58 blockIdentity="midi.drumSound"
+    Vibraslap = 58,
+    //% block="Ride Cymbal 2" enumval=59 blockIdentity="midi.drumSound"
+    RideCymbal2 = 59,
+    //% block="Hi Bongo" enumval=60 blockIdentity="midi.drumSound"
+    HiBongo = 60,
+    //% block="Low Bongo" enumval=61 blockIdentity="midi.drumSound"
+    LowBongo = 61,
+    //% block="Mute Hi Conga" enumval=62 blockIdentity="midi.drumSound"
+    MuteHiConga = 62,
+    //% block="Open Hi Conga" enumval=63 blockIdentity="midi.drumSound"
+    OpenHiConga = 63,
+    //% block="Low Conga" enumval=64 blockIdentity="midi.drumSound"
+    LowConga = 64,
+    //% block="High Timbale" enumval=65 blockIdentity="midi.drumSound"
+    HighTimbale = 65,
+    //% block="Low Timbale" enumval=66 blockIdentity="midi.drumSound"
+    LowTimbale = 66,
+    //% block="High Agogo" enumval=67 blockIdentity="midi.drumSound"
+    HighAgogo = 67,
+    //% block="Low Agogo" enumval=68 blockIdentity="midi.drumSound"
+    LowAgogo = 68,
+    //% block="Cabasa" enumval=69 blockIdentity="midi.drumSound"
+    Cabasa = 69,
+    //% block="Maracas" enumval=70 blockIdentity="midi.drumSound"
+    Maracas = 70,
+    //% block="Short Whistle" enumval=71 blockIdentity="midi.drumSound"
+    ShortWhistle = 71,
+    //% block="Long Whistle" enumval=72 blockIdentity="midi.drumSound"
+    LongWhistle = 72,
+    //% block="Short Guiro" enumval=73 blockIdentity="midi.drumSound"
+    ShortGuiro = 73,
+    //% block="Long Guiro" enumval=74 blockIdentity="midi.drumSound"
+    LongGuiro = 74,
+    //% block="Claves" enumval=75 blockIdentity="midi.drumSound"
+    Claves = 75,
+    //% block="Hi Wood Block" enumval=76 blockIdentity="midi.drumSound"
+    HiWoodBlock = 76,
+    //% block="Low Wood Block" enumval=77 blockIdentity="midi.drumSound"
+    LowWoodBlock = 77,
+    //% block="Mute Cuica" enumval=78 blockIdentity="midi.drumSound"
+    MuteCuica = 78,
+    //% block="Open Cuica" enumval=79 blockIdentity="midi.drumSound"
+    OpenCuica = 79,
+    //% block="Mute Triangle" enumval=80 blockIdentity="midi.drumSound"
+    MuteTriangle = 80,
+    //% block="Open Triangle" enumval=81 blockIdentity="midi.drumSound"
+    OpenTriangle = 81,
+}
---
+++ instruments.ts
@@ -0,0 +1,258 @@
+enum MidiInstrument {
+    //% block="Acoustic Grand Piano" enumval=1
+    AcousticGrandPiano = 1,
+    //% block="Bright Acoustic Piano" enumval=2
+    BrightAcousticPiano = 2,
+    //% block="Electric Grand Piano" enumval=3
+    ElectricGrandPiano = 3,
+    //% block="Honky-tonk Piano" enumval=4
+    HonkytonkPiano = 4,
+    //% block="Electric Piano 1" enumval=5
+    ElectricPiano1 = 5,
+    //% block="Electric Piano 2" enumval=6
+    ElectricPiano2 = 6,
+    //% block="Harpsichord" enumval=7
+    Harpsichord = 7,
+    //% block="Clavi" enumval=8
+    Clavi = 8,
+    //% block="Celesta" enumval=9
+    Celesta = 9,
+    //% block="Glockenspiel" enumval=10
+    Glockenspiel = 10,
+    //% block="Music Box" enumval=11
+    MusicBox = 11,
+    //% block="Vibraphone" enumval=12
+    Vibraphone = 12,
+    //% block="Marimba" enumval=13
+    Marimba = 13,
+    //% block="Xylophone" enumval=14
+    Xylophone = 14,
+    //% block="Tubular Bells" enumval=15
+    TubularBells = 15,
+    //% block="Dulcimer" enumval=16
+    Dulcimer = 16,
+    //% block="Drawbar Organ" enumval=17
+    DrawbarOrgan = 17,
+    //% block="Percussive Organ" enumval=18
+    PercussiveOrgan = 18,
+    //% block="Rock Organ" enumval=19
+    RockOrgan = 19,
+    //% block="Church Organ" enumval=20
+    ChurchOrgan = 20,
+    //% block="Reed Organ" enumval=21
+    ReedOrgan = 21,
+    //% block="Accordion" enumval=22
+    Accordion = 22,
+    //% block="Harmonica" enumval=23
+    Harmonica = 23,
+    //% block="Tango Accordion" enumval=24
+    TangoAccordion = 24,
+    //% block="Acoustic Guitar (nylon)" enumval=25
+    AcousticGuitarNylon = 25,
+    //% block="Acoustic Guitar (steel)" enumval=26
+    AcousticGuitarSteel = 26,
+    //% block="Electric Guitar (jazz)" enumval=27
+    ElectricGuitarJazz = 27,
+    //% block="Electric Guitar (clean)" enumval=28
+    ElectricGuitarClean = 28,
+    //% block="Electric Guitar (muted)" enumval=29
+    ElectricGuitarMuted = 29,
+    //% block="Overdriven Guitar" enumval=30
+    OverdrivenGuitar = 30,
+    //% block="Distortion Guitar" enumval=31
+    DistortionGuitar = 31,
+    //% block="Guitar harmonics" enumval=32
+    GuitarHarmonics = 32,
+    //% block="Acoustic Bass" enumval=33
+    AcousticBass = 33,
+    //% block="Electric Bass (finger)" enumval=34
+    ElectricBassFinger = 34,
+    //% block="Electric Bass (pick)" enumval=35
+    ElectricBassPick = 35,
+    //% block="Fretless Bass" enumval=36
+    FretlessBass = 36,
+    //% block="Slap Bass 1" enumval=37
+    SlapBass1 = 37,
+    //% block="Slap Bass 2" enumval=38
+    SlapBass2 = 38,
+    //% block="Synth Bass 1" enumval=39
+    SynthBass1 = 39,
+    //% block="Synth Bass 2" enumval=40
+    SynthBass2 = 40,
+    //% block="Violin" enumval=41
+    Violin = 41,
+    //% block="Viola" enumval=42
+    Viola = 42,
+    //% block="Cello" enumval=43
+    Cello = 43,
+    //% block="Contrabass" enumval=44
+    Contrabass = 44,
+    //% block="Tremolo Strings" enumval=45
+    TremoloStrings = 45,
+    //% block="Pizzicato Strings" enumval=46
+    PizzicatoStrings = 46,
+    //% block="Orchestral Harp" enumval=47
+    OrchestralHarp = 47,
+    //% block="Timpani" enumval=48
+    Timpani = 48,
+    //% block="String Ensemble 1" enumval=49
+    StringEnsemble1 = 49,
+    //% block="String Ensemble 2" enumval=50
+    StringEnsemble2 = 50,
+    //% block="SynthStrings 1" enumval=51
+    SynthStrings1 = 51,
+    //% block="SynthStrings 2" enumval=52
+    SynthStrings2 = 52,
+    //% block="Choir Aahs" enumval=53
+    ChoirAahs = 53,
+    //% block="Voice Oohs" enumval=54
+    VoiceOohs = 54,
+    //% block="Synth Voice" enumval=55
+    SynthVoice = 55,
+    //% block="Orchestra Hit" enumval=56
+    OrchestraHit = 56,
+    //% block="Trumpet" enumval=57
+    Trumpet = 57,
+    //% block="Trombone" enumval=58
+    Trombone = 58,
+    //% block="Tuba" enumval=59
+    Tuba = 59,
+    //% block="Muted Trumpet" enumval=60
+    MutedTrumpet = 60,
+    //% block="French Horn" enumval=61
+    FrenchHorn = 61,
+    //% block="Brass Section" enumval=62
+    BrassSection = 62,
+    //% block="SynthBrass 1" enumval=63
+    SynthBrass1 = 63,
+    //% block="SynthBrass 2" enumval=64
+    SynthBrass2 = 64,
+    //% block="Soprano Sax" enumval=65
+    SopranoSax = 65,
+    //% block="Alto Sax" enumval=66
+    AltoSax = 66,
+    //% block="Tenor Sax" enumval=67
+    TenorSax = 67,
+    //% block="Baritone Sax" enumval=68
+    BaritoneSax = 68,
+    //% block="Oboe" enumval=69
+    Oboe = 69,
+    //% block="English Horn" enumval=70
+    EnglishHorn = 70,
+    //% block="Bassoon" enumval=71
+    Bassoon = 71,
+    //% block="Clarinet" enumval=72
+    Clarinet = 72,
+    //% block="Piccolo" enumval=73
+    Piccolo = 73,
+    //% block="Flute" enumval=74
+    Flute = 74,
+    //% block="Recorder" enumval=75
+    Recorder = 75,
+    //% block="Pan Flute" enumval=76
+    PanFlute = 76,
+    //% block="Blown Bottle" enumval=77
+    BlownBottle = 77,
+    //% block="Shakuhachi" enumval=78
+    Shakuhachi = 78,
+    //% block="Whistle" enumval=79
+    Whistle = 79,
+    //% block="Ocarina" enumval=80
+    Ocarina = 80,
+    //% block="Lead 1 (square)" enumval=81
+    LeadSquare = 81,
+    //% block="Lead 2 (sawtooth)" enumval=82
+    LeadSawtooth = 82,
+    //% block="Lead 3 (calliope)" enumval=83
+    LeadCalliope = 83,
+    //% block="Lead 4 (chiff)" enumval=84
+    LeadChiff = 84,
+    //% block="Lead 5 (charang)" enumval=85
+    LeadCharang = 85,
+    //% block="Lead 6 (voice)" enumval=86
+    LeadVoice = 86,
+    //% block="Lead 7 (fifths)" enumval=87
+    LeadFifths = 87,
+    //% block="Lead 8 (bass + lead)" enumval=88
+    LeadBassLead = 88,
+    //% block="Pad 1 (new age)" enumval=89
+    PadNewAge = 89,
+    //% block="Pad 2 (warm)" enumval=90
+    PadWarm = 90,
+    //% block="Pad 3 (polysynth)" enumval=91
+    PadPolysynth = 91,
+    //% block="Pad 4 (choir)" enumval=92
+    PadChoir = 92,
+    //% block="Pad 5 (bowed)" enumval=93
+    PadBowed = 93,
+    //% block="Pad 6 (metallic)" enumval=94
+    PadMetallic = 94,
+    //% block="Pad 7 (halo)" enumval=95
+    PadHalo = 95,
+    //% block="Pad 8 (sweep)" enumval=96
+    PadWweep = 96,
+    //% block="FX 1 (rain)" enumval=97
+    FXRain = 97,
+    //% block="FX 2 (soundtrack)" enumval=98
+    FXSoundtrack = 98,
+    //% block="FX 3 (crystal)" enumval=99
+    FXCrystal = 99,
+    //% block="FX 4 (atmosphere)" enumval=100
+    FXAtmosphere = 100,
+    //% block="FX 5 (brightness)" enumval=101
+    FXBrightness = 101,
+    //% block="FX 6 (goblins)" enumval=102
+    FXGoblins = 102,
+    //% block="FX 7 (echoes)" enumval=103
+    FXEchoes = 103,
+    //% block="FX 8 (sci-fi)" enumval=104
+    FXSciFi = 104,
+    //% block="Sitar" enumval=105
+    Sitar = 105,
+    //% block="Banjo" enumval=106
+    Banjo = 106,
+    //% block="Shamisen" enumval=107
+    Shamisen = 107,
+    //% block="Koto" enumval=108
+    Koto = 108,
+    //% block="Kalimba" enumval=109
+    Kalimba = 109,
+    //% block="Bag pipe" enumval=110
+    Bagpipe = 110,
+    //% block="Fiddle" enumval=111
+    Fiddle = 111,
+    //% block="Shanai" enumval=112
+    Shanai = 112,
+    //% block="Tinkle Bell" enumval=113
+    TinkleBell = 113,
+    //% block="Agogo" enumval=114
+    Agogo = 114,
+    //% block="Steel Drums" enumval=115
+    SteelDrums = 115,
+    //% block="Woodblock" enumval=116
+    Woodblock = 116,
+    //% block="Taiko Drum" enumval=117
+    TaikoDrum = 117,
+    //% block="Melodic Tom" enumval=118
+    MelodicTom = 118,
+    //% block="Synth Drum" enumval=119
+    SynthDrum = 119,
+    //% block="Reverse Cymbal" enumval=120
+    ReverseCymbal = 120,
+    //% block="Guitar Fret Noise" enumval=121
+    GuitarFretNoise = 121,
+    //% block="Breath Noise" enumval=122
+    BreathNoise = 122,
+    //% block="Seashore" enumval=123
+    Seashore = 123,
+    //% block="Bird Tweet" enumval=124
+    BirdTweet = 124,
+    //% block="Telephone Ring" enumval=125
+    TelephoneRing = 125,
+    //% block="Helicopter" enumval=126
+    Helicopter = 126,
+    //% block="Applause" enumval=127
+    Applause = 127,
+    //% block="Gunshot" enumval=128
+    Gunshot = 128
+}
