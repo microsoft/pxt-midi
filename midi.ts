c3991b9a996a565bb1c7cba8e692fc30cbcabd9b
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-10-08 21:22:37 -0700

    removing advanced

--- midi.ts
+++ midi.ts
@@ -52,7 +52,7 @@ namespace midi {
      * Sets the transport mechanism to send MIDI commands
      * @param newTransport current transport
      */
-    //% advanced=true
+    //% advanced=true blockHidden=1
     export function setTransport(newTransport: (data: Buffer) => void) {
         transport = newTransport;
     }
@@ -61,7 +61,7 @@ namespace midi {
      * Sends a MIDI message
      * @param data 1,2 or 3 numbers
      */
-    //% advanced=true
+    //% advanced=true blockHidden=1
     export function sendMessage(data: number[]) {
         if (!transport) return;
 
@@ -76,7 +76,7 @@ namespace midi {
     * Send raw MIDI messages via serial
     */
     //% blockId=midi_raw_serial_transport block="midi use raw serial"
-    //% advanced=true
+    //% weight=2
     export function useRawSerial() {
         function send(data: Buffer): void {
             serial.writeBuffer(data)
@@ -88,7 +88,7 @@ namespace midi {
      * Send human readable MIDI messages via serial
      */
     //% blockId=midi_serial_transport block="midi use serial"
-    //% advanced=true
+    //% weight=1
     export function useSerial() {
         function send(data: Buffer): void {
             // waiting for beta
@@ -357,7 +357,7 @@ namespace midi {
      * @param frequency
      */
     //% blockId=midi_frequency_to_key block="key at %frequency=device_note" useEnumVal=1
-    //% advanced=true shim=midi::frequencyToKey
+    //% shim=midi::frequencyToKey weight=4 blockGap=8
     export function frequencyToKey(frequency: number): number {
         const notes = [8, 9, 9, 10, 10, 11, 12, 12, 13, 14, 15, 15, 16, 17, 18, 19, 21, 22, 23, 24, 26, 28, 29, 31, 33, 35, 37, 39, 41, 44, 46, 49, 52, 55, 58, 62, 65, 69, 73, 78, 82, 87, 92, 98, 104, 110, 117, 123, 131, 139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523, 554, 587, 622, 659, 698, 740, 784, 831, 880, 932, 988, 1047, 1109, 1175, 1245, 1319, 1397, 1480, 1568, 1661, 1760, 1865, 1976, 2093, 2217, 2349, 2489, 2637, 2794, 2960, 3136, 3322, 3520, 3729, 3951, 4186, 4435, 4699, 4978, 5274, 5588, 5920, 6272, 6645, 7040, 7459, 7902, 8372, 8870, 9397, 9956, 10548, 11175, 11840, 12544];
         let left = 0, right = 0x7f, mid = 69;
